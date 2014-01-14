313561ee3f5c42b40e80c16595d5384a4c75382b
kg <kg@teuto.net>
2014-01-14 16:33:01 +0100

    Added remaining files from https://github.com/windelicato/dotfiles

--- afterCheckout.sh
+++ afterCheckout.sh
@@ -1,3 +1,36 @@
 #!/bin/sh
 
-apt-get install unclutter  rxvt-unicode  compton
+# ensure ~/.fonts exists
+if [ ! -d ~/.fonts ]; then
+	mkdir ~/.fonts
+fi
+
+tar xzf gohufont-2.0.tar.gz
+tar xzf artwiz-latin1-1.1.tgz
+
+# create the pcf files
+bdftopcf -o ~/.fonts/lemon.pcf ~/.fonts/lemon.bdf
+bdftopcf -o ~/.fonts/uushi.pcf ~/.fonts/uushi.bdf
+bdftopcf -o ~/.fonts/tewi/Tewi-bold-11.pcf ~/.fonts/tewi/Tewi-bold-11.bdf
+bdftopcf -o ~/.fonts/tewi/Tewi-bold-11.pcf ~/.fonts/tewi/Tewi-normal-11.bdf
+
+# install the fonts
+#mkfontdir ~/.fonts
+#mkfontdir ~/.fonts/gohufont-2.0
+mkfontdir ~/.fonts/artwiz-latin1
+#mkfontscale ~/.fonts
+#mkfontscale ~/.fonts/gohufont-2.0
+mkfontscale ~/.fonts/artwiz-latin1
+#xset +fp ~/.fonts
+#xset +fp ~/.fonts/gohufont-2.0
+xset +fp ~/.fonts/artwiz-latin1
+xset fp rehash
+fc-cache -fv
+#fc-cache -fv ~/.fonts/gohufont-2.0
+fc-cache -fv ~/.fonts/artwiz-latin1
+
+sudo dpkg-reconfigure fontconfig
+sudo apt-get install unclutter  rxvt-unicode  compton ttf-dejavu-core
+
+#Fixed the problem, had to edit xorg.conf to include the 
+#artwiz-latin1-fonts folder. Works fine now, thanks for the help.
